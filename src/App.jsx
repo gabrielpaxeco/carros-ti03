a25849213f6ca4d6cc70477b3fc3a885b90c2521
Gabriel Pacheco <gabrielenaty0923@gmail.com>
2024-04-01 02:38:39 -0300

    projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ export default function App() {
   return(
     <main className="py-20 px-6 min-h-svh bg-offwhitebgm antialiased lg:flex justify-center
     items-center ">
-      <section className="lg:flex max-w-[920px]">
+      <div className="lg:flex max-w-[920px]">
         <Sedans/>
         <Suvs/>
         <Luxuosos/>
-      </section>
+      </div>
     </main>
   )
 }
